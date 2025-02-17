908391110c9d17c60a6db023a9d304242d7500cf
guillear <azzayu@PCAzzayuReborn.>
2025-02-17 09:57:03 +0100

    started working on the first screen system for displaying

--- main.c
+++ main.c
@@ -4,10 +4,17 @@
 
 
 
-char FULL_BLOCK[] = "██";
+char FULL_BLOCK[] = "██"; 
 char THREE_QUART_BLOCK[] = "▓▓";
 char HALF_BLOCK[] = "▒▒";
 char QUART_BLOCK[] = "░░";
+char VERT_WALL_LEFT[] = "║ ";
+char VERT_WALL_RIGHT[] = " ║";
+char HORI_WALL[] = "══";
+char TOP_LEFT_CORNER[] = " ╔";
+char TOP_RIGHT_CORNER[] = "╗ ";
+char BOTTOM_LEFT_CORNER[] = " ╚";
+char BOTTOM_RIGHT_CORNER[] = "╝ ";
 
 
 char BLACK[] = "\033[0;30m";
@@ -28,36 +35,118 @@ char BRIGHT_CYAN[] = "\033[0;96m";
 char BRIGHT_WHITE[] = "\033[0;97m";
 
 
-struct struct_pixel_s {
-	int layer[50];
+typedef struct pixel_s {
+	int layer[100];
 } pixel;
 
 
+void print_screen(pixel **pixel_mat, int width, int height){
+	/*
+	each layer corresponds to something different with things closer to 0 more important to display (hence they get displayed)
+	0 => player
+	1 - 7 => border
+
+
+	50 => background of play area
+	none to 1 => general black background (normal terminal screen)
+	*/
+	for (int y = 0; y < height; y++){
+		for (int x = 0; x < width; x++){
+			if (pixel_mat[y][x].layer[0] == 1) {
+				printf("%s%s", YELLOW, FULL_BLOCK);
+			} else if (pixel_mat[y][x].layer[1] == 1) {
+				printf("%s%s", WHITE, VERT_WALL_LEFT);
+			} else if (pixel_mat[y][x].layer[2] == 1) {
+				printf("%s%s", WHITE, VERT_WALL_RIGHT);
+			} else if (pixel_mat[y][x].layer[3] == 1) {
+				printf("%s%s", WHITE, HORI_WALL);
+			} else if (pixel_mat[y][x].layer[4] == 1) {
+				printf("%s%s", WHITE, TOP_LEFT_CORNER);
+			} else if (pixel_mat[y][x].layer[5] == 1) {
+				printf("%s%s", WHITE, TOP_RIGHT_CORNER);
+			} else if (pixel_mat[y][x].layer[6] == 1) {
+				printf("%s%s", WHITE, BOTTOM_LEFT_CORNER);
+			} else if (pixel_mat[y][x].layer[7] == 1) {
+				printf("%s%s", WHITE, BOTTOM_RIGHT_CORNER);
+			} else if (pixel_mat[y][x].layer[50] == 1) {
+				printf("%s%s", BRIGHT_BLUE, FULL_BLOCK);
+			} else {
+				printf("%s%s", BLACK, FULL_BLOCK);
+			}
+		}
+		printf("\n");
+	}
+	fflush(stdout);
+}
+
+
+pixel **init_screen(int width, int height){
+	pixel **screen = malloc(sizeof(pixel*) * height);
+	for (int y = 0; y < height; y++){
+		screen[y] = malloc(sizeof(pixel) * width);
+		for (int x = 0; x < width; x++){
+			for (int layer = 0; layer < 100; layer ++){
+				screen[y][x].layer[layer] = 0;
+			}
+			screen[y][x].layer[50] = 1;
+		}
+	}
+	for (int x = 1; x < width - 1; x++){
+		screen[0][x].layer[3] = 1;
+		screen[height - 1][x].layer[3] = 1;
+	}
+	for (int y = 1; y < height - 1; y++){
+		screen[y][0].layer[2] = 1;
+		screen[y][width - 1].layer[1] = 1;
+	}
+	screen[0][0].layer[4] = 1;
+	screen[0][width - 1].layer[5] = 1;
+	screen[height - 1][0].layer[6] = 1;
+	screen[height - 1][width - 1].layer[7] = 1;
+	return screen;
+}
+
+
+void default_test_screen(){
+	system("clear");
+	printf("%s %s%s%s%s\n", BLACK, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", RED, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", GREEN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", YELLOW, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BLUE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", MAGENTA, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", CYAN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", WHITE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_BLACK, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_RED, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_GREEN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_YELLOW, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_BLUE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_MAGENTA, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_CYAN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+	printf("%s %s%s%s%s\n", BRIGHT_WHITE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
+		
+	fflush(stdout);
+}
+
+
+void free_screen(pixel **pixel_mat, int width, int height){
+	for (int y = 0; y < height; y++){
+		free(pixel_mat[y]);
+	}
+	free(pixel_mat);
+}
+
 
 int main(){
+
+	int width = 50;
+	int height = 30;
  
-	while (1) 
-	{
-		system("clear");
-		printf("%s %s%s%s%s\n", BLACK, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", RED, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", GREEN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", YELLOW, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BLUE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", MAGENTA, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", CYAN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", WHITE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_BLACK, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_RED, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_GREEN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_YELLOW, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_BLUE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_MAGENTA, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_CYAN, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		printf("%s %s%s%s%s\n", BRIGHT_WHITE, FULL_BLOCK, THREE_QUART_BLOCK, HALF_BLOCK, QUART_BLOCK);
-		
-		fflush(stdout);
-		sleep(1);
-	}
+	pixel **screen = init_screen(width, height);
+	screen[10][15].layer[0] = 1;
+	print_screen(screen, width, height);
+
+	free_screen(screen, width, height);
 	return EXIT_SUCCESS;
 }
