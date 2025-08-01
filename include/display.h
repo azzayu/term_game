ec1b09e1cb3e853d7062ef866aed4ec6b18da6ab
azzayu <armand.guillet@gmail.com>
2025-08-01 19:42:49 +0100

    added empty line at the end of files

--- include/display.h
+++ include/display.h
@@ -5,20 +5,14 @@
 
 pixel **init_screen(int width, int height, screen_section play_area, screen_section enemy_locations[3]);
 
-
 void default_test_screen();
-
 
 void free_screen(pixel **pixel_mat, int height);
 
-
 void print_screen(pixel **pixel_mat, int width, int height, text_section** all_text);
-
 
 text_section *init_text(int x_min, int x_max, int y, char* text, int length);
 
-
 screen_section init_screen_section(int x_min, int y_min, int x_max, int y_max);
-
 
 #endif
